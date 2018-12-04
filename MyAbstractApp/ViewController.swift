5c18492c58eb78ed2a31a99b780460d1c9e4af95
T4Nn3rB0w3N <tbow1382@740s-pl-103.local>
2018-12-04 08:42:17 -0700

    created single-view app, and did gitignore

---
+++ MyAbstractApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAbstractApp
+//
+//  Created by Bowen, Tanner on 12/4/18.
+//  Copyright © 2018 Bowen, Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
